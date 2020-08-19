70f9ae2b09462ab44bf1ea3e80f33873bfe3b81f
RobertoBerante <berante@gmail.com>
2020-08-19 17:59:37 -0300

    Origin update.

--- login.php
+++ login.php
@@ -3,9 +3,11 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>HM Login</title>
+  <link rel="stylesheet" type="text/css" href="styles/normalize.css">
+  <link rel="stylesheet" type="text/css" href="styles/style.css">
+  <title>Login</title>
 </head>
 <body>
-  <h1>HM Login</h1>
+  <h1>Login</h1>
 </body>
 </html>
