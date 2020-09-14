2aad2dd16203dae65d43c3476fd930757f3bc165
Tato Berante <tato@berante.com.ar>
2020-09-14 10:02:22 -0300

    Minor changes.

--- core/config.php
+++ core/config.php
@@ -3,7 +3,7 @@ $appname = "APP";
 $logo = "../v/img/logo.png";
 $db = "basep";
 $dbusr = "root";
-$dbkey = "Hijo09";
+$dbkey = "Nurgle2020";
 $ftp = "ftp://200.110.219.83/   ";
 $charset = "utf8mb4_unicode_ci";
 ?>
