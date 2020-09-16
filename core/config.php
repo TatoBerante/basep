80ad6ed460c487a2c8a5ac53fb0dfa5b6e7805f3
Tato Berante <tato@berante.com.ar>
2020-09-16 12:24:55 -0300

    Flushed branch to allow gitignore new files.

--- core/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$appname = "APP";
-$logo = "../v/img/logo.png";
-$db = "basep";
-$dbusr = "root";
-$dbkey = "Hijo09";
-$ftp = "ftp://200.110.219.83/   ";
-$charset = "utf8mb4_unicode_ci";
-?>
