976db1f2205730ac204594a7e26d3208246e28e9
RobertoBerante <berante@gmail.com>
2020-09-23 19:30:43 -0300

    Fixed DB file for windows desktop.

---
+++ core/db-windows.sql
@@ -0,0 +1,199 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.5deb2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Sep 23, 2020 at 10:05 AM
+-- Server version: 8.0.21-0ubuntu0.20.04.4
+-- PHP Version: 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+
+--
+-- Database: `basep`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `clientes`
+--
+
+CREATE TABLE `clientes` (
+  `id_cliente_sys` int UNSIGNED NOT NULL,
+  `id_cliente` varchar(6) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `empresa` varchar(2) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `cliente` varchar(200)  CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `tipo_cliente` varchar(100)  CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `condicion_pago` varchar(3)  CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `condicion` varchar(100)  CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `usuario_id` int UNSIGNED NOT NULL,
+  `usuario_nombre` varchar(100)  CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `usuario_apellido` varchar(100)  CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `usuario_nick` varchar(100)  CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `usuario_key` varchar(50)  CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `usuario_hry` tinyint UNSIGNED NOT NULL DEFAULT '100',
+  `usuario_el` tinyint UNSIGNED NOT NULL DEFAULT '1' COMMENT '0: desactivado / 1:activo'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+INSERT INTO `usuarios` (`usuario_id`, `usuario_nombre`, `usuario_apellido`, `usuario_nick`, `usuario_key`, `usuario_hry`, `usuario_el`) VALUES
+(1, 'Tato', 'Berante', 'admin', 'xpxrulz', 1, 1),
+(4, 'falso', 'berante', 'user1234', 'poiupoiux', 100, 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`id_cliente_sys`),
+  ADD UNIQUE KEY `unique_index` (`id_cliente`,`empresa`);
+
+--
+-- Indexes for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`usuario_id`),
+  ADD UNIQUE KEY `usuario_nick` (`usuario_nick`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `clientes`
+--
+ALTER TABLE `clientes`
+  MODIFY `id_cliente_sys` int UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `usuario_id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+--
+-- Table structure for table `medicos`
+--
+
+CREATE TABLE `medicos` (
+  `id_medico_sys` int UNSIGNED NOT NULL,
+  `id_medico` varchar(6)  CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `medico` varchar(100)  CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `medicos`
+--
+ALTER TABLE `medicos`
+  ADD PRIMARY KEY (`id_medico_sys`),
+  ADD UNIQUE KEY `id_medico` (`id_medico`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `medicos`
+--
+ALTER TABLE `medicos`
+  MODIFY `id_medico_sys` int UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+--
+-- Table structure for table `productos`
+--
+
+CREATE TABLE `productos` (
+  `id_producto_sys` int UNSIGNED NOT NULL,
+  `empresa` varchar(2)  CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `id_producto` varchar(20)  CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `descripcion` varchar(200)  CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`id_producto_sys`),
+  ADD UNIQUE KEY `empresa` (`empresa`,`id_producto`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `productos`
+--
+ALTER TABLE `productos`
+  MODIFY `id_producto_sys` int UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+--
+-- Table structure for table `vendedores`
+--
+
+CREATE TABLE `vendedores` (
+  `id_vendedor_sys` int UNSIGNED NOT NULL,
+  `id_vendedor` varchar(6)  CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `vendedor` varchar(100)  CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `vendedores`
+--
+ALTER TABLE `vendedores`
+  ADD PRIMARY KEY (`id_vendedor_sys`),
+  ADD UNIQUE KEY `id_vendedor` (`id_vendedor`,`vendedor`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `vendedores`
+--
+ALTER TABLE `vendedores`
+  MODIFY `id_vendedor_sys` int UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
