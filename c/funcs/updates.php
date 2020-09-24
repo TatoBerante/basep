07a1e713e05ea1384138ee438f5dd5dbe8e8ce8c
Tato Berante <tato@berante.com.ar>
2020-09-24 18:35:29 -0300

    Added cirugias updates. All sources files are now UTF8.

--- c/funcs/updates.php
+++ c/funcs/updates.php
@@ -143,9 +143,6 @@ function updateVendedores () {
   mysqli_close($mysqli);
   return $new;
 }
-function updateCirugias () {
-  return rand (0, 500);
-}
 function updateProductos () {
   $mysqli = mysqli_conn();
   $txt_file = file_get_contents('../update-source/Productos.csv');
@@ -174,6 +171,76 @@ function updateProductos () {
         }
         else {
           mysqli_stmt_bind_param ($stmt, "sss", $empresa, $id_producto, $descripcion);
+          if (mysqli_stmt_execute ($stmt)) {
+            $new++;
+            mysqli_stmt_close($stmt);
+          }
+          else {
+            // error report (fallo por cliente ya registrado previamente):
+            // echo "<p>Data:  ".$empresa." / ".$id_producro." / ".$descripcion.": ".mysqli_stmt_error($stmt)."</p>";
+            $errors++;
+            
+          }
+        }
+      }
+    }
+  }
+  mysqli_close($mysqli);
+  return $new;
+}
+function updateCirugias () {
+  // Comercial_filtro_fechasPV+Recibo2
+  $mysqli = mysqli_conn();
+  $txt_file = file_get_contents('../update-source/Comercial_filtro_fechasPV+Recibo2.csv');
+  $rows = explode("\n", $txt_file);
+  array_shift($rows); // Quita el primer valor del array (evita fila de nombres)
+  $errors = 0;
+  $new = 0;
+  foreach($rows as $row => $data) {
+    if ($row > 0) { // Evita la segunda línea (+-----+-----+)
+      //get row data
+      $row_data = explode('|', $data);
+      if (count($row_data) > 1) {
+        $recno = trim ($row_data[0]);
+        $filial = trim ($row_data[1]);
+        $nro_presupuesto = trim ($row_data[2]);
+        $orden_compra = trim ($row_data[3]);
+        $fecha_pedido_venta = trim ($row_data[4]);
+        $nro_pedido_de_venta = trim ($row_data[5]);
+        $tipo_de_venta = trim ($row_data[6]);
+        $fecha_cx = trim ($row_data[7]);
+        $nro_cirugia = trim ($row_data[8]);
+        $cod_medico = trim ($row_data[9]);
+        $nombre_paciente = trim ($row_data[10]);
+        $fecha_emision = trim ($row_data[11]);
+        $comprobante = trim ($row_data[12]);
+        $serie = trim ($row_data[13]);
+        $cod_cliente = trim ($row_data[14]);
+        $cod_institucion = trim ($row_data[15]);
+        $cod_vendedor = trim ($row_data[16]);
+        $nombre_vendedor = trim ($row_data[17]);
+        $producto = trim ($row_data[18]);
+        $descripcion = trim ($row_data[19]);
+        $item = trim ($row_data[20]);
+        $cantidad = trim ($row_data[21]);
+        $precio_venta = trim ($row_data[22]);
+        $valor_total1 = trim ($row_data[23]);
+        $valor_total2 = trim ($row_data[24]);
+        $serierecibo = trim ($row_data[25]);
+        $recibo = trim ($row_data[26]);
+
+        $sql = "INSERT INTO cirugias (recno, filial, nro_presupuesto, orden_compra, fecha_pedido_venta, nro_pedido_de_venta, tipo_de_venta, fecha_cx, nro_cirugia, cod_medico, nombre_paciente, fecha_emision, comprobante, serie, cod_cliente, cod_institucion, cod_vendedor, nombre_vendedor, producto, descripcion, item, cantidad, precio_venta, valor_total1, valor_total2, serierecibo, recibo)
+                  VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        $stmt = mysqli_stmt_init ($mysqli);
+        if (!mysqli_stmt_prepare ($stmt, $sql)) {
+            // error report:
+            echo "<p>";
+            print_r (mysqli_stmt_error($stmt));
+            echo " Cliente (mysqli_stmt_prepare): ".$cliente."</p>*****";
+            $errors++;
+        }
+        else {
+          mysqli_stmt_bind_param ($stmt, "sssssssssssssssssssssssssss", $recno, $filial, $nro_presupuesto, $orden_compra, $fecha_pedido_venta, $nro_pedido_de_venta, $tipo_de_venta, $fecha_cx, $nro_cirugia, $cod_medico, $nombre_paciente, $fecha_emision, $comprobante, $serie, $cod_cliente, $cod_institucion, $cod_vendedor, $nombre_vendedor, $producto, $descripcion, $item, $cantidad, $precio_venta, $valor_total1, $valor_total2, $serierecibo, $recibo);
           if (mysqli_stmt_execute ($stmt)) {
             $new++;
             mysqli_stmt_close($stmt);
